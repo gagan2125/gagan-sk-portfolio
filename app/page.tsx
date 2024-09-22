088c1c0c42ef54cf6c1c820f2887104056d9c459
gagan2125 <gagansk2125@gmail.com>
2024-09-22 14:59:30 +0530

    innital commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+import { FaHome } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
